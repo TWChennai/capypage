634d22ee8d43fa5caa34dcc8cf10d03a5b6776fc
Selva <k.n.selvakumar@gmail.com>
2013-09-18 11:54:07 +0530

    Added readme for sample, using new rspec expectation syntax

---
+++ examples/simple/README.md
@@ -0,0 +1,7 @@
+# Capypage Simple rspec example
+
+
+## How to run
+
+    gem 'capypage'
+    rspec
--- examples/simple/spec/simple_spec.rb
+++ examples/simple/spec/simple_spec.rb
@@ -6,10 +6,10 @@ describe 'Simple example', :type => :feature do
 
   it 'should show capybara results' do
     search_page.load
-    search_page.search "Capybara"
+    search_page.search 'Capybara'
 
-    capybara_result = search_results_page.results.find_by_text "jnicklas"
-    capybara_result.link[:href].should == 'https://github.com/jnicklas/capybara'
-    capybara_result.snippet.should have_text "Capybara helps you test web applications"
+    capybara_result = search_results_page.results.find_by_text 'jnicklas'
+    expect(capybara_result.link[:href]).to eq('https://github.com/jnicklas/capybara')
+    expect(capybara_result.snippet).to have_text('Capybara helps you test web applications')
   end
 end
